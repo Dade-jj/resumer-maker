5c9b62138cb2877eb7a9e105a459b16f24aab049
unknown <547806059@qq.com>
2018-05-28 12:49:38 +0800

    config

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/resumer-maker/dist/',
+    assetsPublicPath: './',
     proxyTable: {},
 
     // Various Dev Server settings
